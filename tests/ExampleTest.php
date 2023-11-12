cf81953527f4b43e16880e642c85b235fafe5fbb
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2023-11-12 22:12:20 +0100

    add example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Acme\TestBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        /**
+         * do your testing here
+         */
+    }
+}
